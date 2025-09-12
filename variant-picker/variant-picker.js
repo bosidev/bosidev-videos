2823ae2a91e5775d3ab026b91fde5693d1bac1c6
bosidev <info@bosidev.com>
2025-09-12 14:04:17 +0200

    Add variant picker video

---
+++ variant-picker/variant-picker.js
@@ -0,0 +1,52 @@
+class VariantSelector extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  get sectionId() {
+    return this.getAttribute("data-section-id");
+  }
+
+  connectedCallback() {
+    this.selectors = this.querySelectorAll("input[type='radio']");
+    this.handleChange = this.handleChange.bind(this);
+
+    this.selectors.forEach((selector) => {
+      selector.addEventListener("change", this.handleChange);
+    });
+  }
+
+  disconnectedCallback() {
+    this.selectors.forEach((selector) => {
+      selector.removeEventListener("change", this.handleChange);
+    });
+  }
+
+  handleChange(event) {
+    const url = `${window.location.pathname}?variant=${event.currentTarget.value}&section_id=${this.sectionId}`;
+
+    fetch(url)
+      .then((response) => {
+        return response.text();
+      })
+      .then((data) => {
+        const tempDiv = document.createElement("div");
+        tempDiv.innerHTML = data;
+        document.querySelector(".product-content").innerHTML =
+          tempDiv.querySelector(".product-content").innerHTML;
+        document.querySelector(".product-images").innerHTML =
+          tempDiv.querySelector(".product-images").innerHTML;
+
+        const newUrl = new URL(url, window.location.origin);
+        newUrl.searchParams.delete("section_id");
+        window.history.pushState({}, "", newUrl.toString());
+      })
+      .catch((error) => {
+        if (error.name !== "AbortError") {
+          console.error("Error fetching variant:", error);
+        }
+      });
+  }
+}
+
+customElements.define("variant-selector", VariantSelector);
